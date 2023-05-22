d410d98ac5d12ff1e07788be1ea480dabb91e10a
Fazal Rehman <115558578+fazal133@users.noreply.github.com>
2023-05-22 11:17:43 -0700

    My Portfolio Website

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Fazal Rehman</span> I am a skilled web developer and have two years of experience in this field. I am known for attention to detail, strong work ethic, and dedication to delivering quality results. I'm passionate about staying up-to-date with the latest web development trends and technologies, and I'm always looking for new ways to improve my craft. I have excellent problem solving skills and am committed to understanding the unique needs of my clients to provide customized solutions that meet their needs. With my strong communication skills, I am an excellent colleague and team player, making me a valuable asset to any web development project."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
